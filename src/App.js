3e79173340ae756e31c974efea1defbf3dc52119
MitGanesh <ganeshmittha88@gmail.com>
2022-07-22 13:40:07 +0530

    Router setup for category

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 export default class App extends Component {
@@ -10,8 +15,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News country="in" category="sports"/>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<News key="general" country="in" category="general" />}/>
+            <Route path="/general" element={<News key="general" country="in" category="general" />}/>
+            <Route path="/business" element={<News key="business" country="in" category="business" />}/>
+            <Route path="/entertainment" element={<News key="entertainment" country="in" category="entertainment" />}/>
+            <Route path="/health" element={<News key="health" country="in" category="health" />}/>
+            <Route path="/science" element={<News key="science" country="in" category="science" />}/>
+            <Route path="/sports" element={<News key="sports" country="in" category="sports" />}/>
+            <Route path="/technology" element={<News key="technology" country="in" category="technology" />}/>
+          </Routes>
+        </BrowserRouter>
       </div>
     )
   }
